c7bad773b26ab27407d820aaae73de3b5cd921f6
Projojoboy <22048@ma-web.nl>
2018-03-19 11:18:28 +0100

    Added project

---
+++ Assets/Scripts/Domovoi/DomovoiStateMachine.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class DomovoiStateMachine : MonoBehaviour {
+
+    private enum _eStates { Walk, Idle }
+    private _eStates _state = _eStates.Walk;
+    private NavMeshAgent _agent;
+
+    [SerializeField] private Vector3 _goal;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+    }
+
+    private void Update()
+    {
+        StateMachine();
+    }
+
+    private void StateMachine()
+    {
+        switch (_state) {
+            case _eStates.Walk:
+                _agent.destination = _goal;
+                break;
+            case _eStates.Idle:
+
+                break;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(_goal, 1);
+    }
+}
