2282f1b36ec5d4015c1880cbe32f03e5b2d0c852
BuildTools <unconfigured@null.spigotmc.org>
2018-03-22 14:07:01 +0100

    InteractionWithDomovoi added

---
+++ Assets/Scripts/Interaction/InteractableWithDomovoi.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableWithDomovoi : MonoBehaviour {
+
+    [SerializeField] private DomovoiStateMachine _domovoi;
+    [SerializeField] private bool _debug = false;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (_debug) Debug.Log("Magnitude: " + collision.relativeVelocity.magnitude);
+        if (collision.relativeVelocity.magnitude > 4)
+        {
+            _domovoi.Target(gameObject);
+            if (_debug) Debug.Log("Magnitude hit");
+        }
+    }
+}
