5d412ce0e3695fd1dcd86f85ee58fb7927a01dc2
wesselvink <22092@ma-web.nl>
2018-03-26 14:13:12 +0200

    Zaklampje

---
+++ Assets/Scripts/Flashlight/Flashlight_OnOff.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flashlight_OnOff : MonoBehaviour {
+	private SteamVR_Controller.Device device;
+	public Light Flashlight;
+	//public AudioSource audioSource;
+
+	//public AudioClip soundOn;
+	//public AudioClip soundOff;
+
+	private bool isActive;
+
+
+	void Start () {
+		isActive = true;	
+	}
+	
+
+	void Update () {
+		if (device.GetPressDown(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger )){
+			Debug.Log ("Up");
+			if (isActive == false) {
+				Flashlight.enabled = true;
+				isActive = true;
+
+				//audioSource.PlayOneShot (soundOn);
+			} else if (isActive == true) {
+				Flashlight.enabled = true;
+				isActive = false;
+
+				//audioSource.PlayOneShot (soundOff);
+			}
+		}
+	}
+}
