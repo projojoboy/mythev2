09fb0d0d5abcaba085a0a672669fc419de3b3240
Projojoboy <22048@ma-web.nl>
2018-03-26 13:34:10 +0200

    Added the loot system

---
+++ Assets/Scripts/LootSystem/Loot.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Loot : MonoBehaviour {
+
+    [SerializeField] int lootValue;
+
+    [SerializeField] GameObject particle;
+
+    LootController lc;  // GameController holding the LootController script
+
+	// Use this for initialization
+	void Start () {
+        lc = GameObject.Find("GameController").GetComponent<LootController>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider coll)
+    {
+        if(coll.tag == "Moneybag")
+        {
+            Debug.Log("collision");
+            lc.AddLootPoints(lootValue);
+            Instantiate(particle);
+        }
+    }
+}
