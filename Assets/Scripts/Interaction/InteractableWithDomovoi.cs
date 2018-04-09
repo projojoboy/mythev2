fa49296eb6b4c9c632b9d0e400642a903a3441bb
BuildTools <unconfigured@null.spigotmc.org>
2018-04-09 14:44:24 +0200

    StateMachine Rework

--- Assets/Scripts/Interaction/InteractableWithDomovoi.cs
+++ Assets/Scripts/Interaction/InteractableWithDomovoi.cs
@@ -2,21 +2,14 @@
 
 public class InteractableWithDomovoi : MonoBehaviour {
 
-    private DomovoiController _domovoiController;
-
     [SerializeField] private bool _debug = false;
-
-    private void Awake()
-    {
-        _domovoiController = GameObject.Find(ObjectReferences.domovoi).GetComponent<DomovoiController>();
-    }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (_debug) Debug.Log("Magnitude: " + collision.relativeVelocity.magnitude);
         if (collision.relativeVelocity.magnitude > 4)
         {
-            _domovoiController.Target(gameObject);
+            DomovoiController.Instance.Target(gameObject);
             if (_debug) Debug.Log("Magnitude hit");
         }
     }
