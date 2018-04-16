90d72d6d323a482ca08d80520a6dcca6d2c11a82
Projojoboy <22048@ma-web.nl>
2018-04-16 22:29:13 +0200

    Added Main Menu

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuController : MonoBehaviour {
+
+    [SerializeField] private GameObject quitConfirmationScreen;
+    [SerializeField] private GameObject mainMenu;
+
+    public void ExitGame()
+    {
+        quitConfirmationScreen.SetActive(true);
+        mainMenu.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        //Load scene(moet nog scene hebben)
+        //SceneManager.LoadScene();
+        Debug.Log("StartGame!");
+    }
+
+    public void ConfirmQuit(bool quit)
+    {
+        if (quit)
+            Application.Quit();
+        else if (!quit)
+        {
+            quitConfirmationScreen.SetActive(false);
+            mainMenu.SetActive(true);
+        }
+    }
+}
