cf83292d99aadf199a3cb025cdcacdd7f059ef53
larsplomp27 <15629@ma-web.nl>
2018-04-20 15:17:54 +0100

    AllProps"

---
+++ Assets/Assets/Scripts/Domovoi/State.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+public abstract class State : MonoBehaviour
+{
+    public virtual void StateEnter() { }
+    public virtual void StateLeave() { }
+    public abstract bool Reason();
+    public abstract void StateUpdate();
+}
